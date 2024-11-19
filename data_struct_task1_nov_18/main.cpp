210ee0e52631bcd6093a72a19a2f8d0caf83ec92
colt <colt.github@outlook.com>
2024-11-19 10:48:02 +0100

    Simple test of this single linked list

--- data_struct_task1_nov_18/main.cpp
+++ data_struct_task1_nov_18/main.cpp
@@ -5,5 +5,20 @@ int main(void)
 {
     LinkedList ll;
 
+    for (int i = 0; i < 15; i++)
+    {
+        ll.Append(i);
+    }
+
+    std::cout << ll.GetNodeByIndex(5)->GetValue() << std::endl;
+
+    ll.RemoveNodeByIndex(5);
+
+    std::cout << ll.GetNodeByIndex(5)->GetValue() << std::endl;
+
+    ll.InsertAfterIndex(10, 4);
+
+    std::cout << ll.GetNodeByIndex(5)->GetValue() << std::endl;
+
     return 0;
 }
